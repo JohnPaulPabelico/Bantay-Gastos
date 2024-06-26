b98a9481b877dfc45aef7a4a056a213371575118
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-26 18:01:23 +0800

    Added a few authentication related methods

--- web-app/src/app/shared/auth.service.ts
+++ web-app/src/app/shared/auth.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  FacebookAuthProvider,
+} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +15,16 @@ export class AuthService {
 
   login(email: string, password: string) {
     this.fireauth.signInWithEmailAndPassword(email, password).then(
-      () => {
+      (res) => {
         localStorage.setItem('token', 'true');
-        this.router.navigate(['/dashboard']);
+
+        if (res.user?.emailVerified == true) {
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.router.navigate(['/verify']);
+        }
       },
+
       (err) => {
         alert('Something went wrong: ' + err.message);
         this.router.navigate(['/login']);
@@ -23,8 +34,9 @@ export class AuthService {
 
   register(email: string, password: string) {
     this.fireauth.createUserWithEmailAndPassword(email, password).then(
-      () => {
+      (res) => {
         alert('Registration successful');
+        this.sendEmailVerification(res.user);
         this.router.navigate(['/login']);
       },
       (err) => {
@@ -45,4 +57,62 @@ export class AuthService {
       }
     );
   }
+
+  forgotPassword(email: string) {
+    this.fireauth.sendPasswordResetEmail(email).then(
+      () => {
+        this.router.navigate(['/forgot/link']);
+      },
+      (err) => {
+        alert(err.message);
+      }
+    );
+  }
+
+  sendEmailVerification(user: any) {
+    user.sendEmailVerification().then(
+      (res: any) => {
+        this.router.navigate(['/verify']);
+      },
+      (err: any) => {
+        alert(err.message);
+      }
+    );
+  }
+
+  googleSignIn() {
+    return this.fireauth.signInWithPopup(new GoogleAuthProvider()).then(
+      (res) => {
+        this.router.navigate(['/dashboard']);
+        localStorage.setItem('token', JSON.stringify(res.user?.uid));
+      },
+      (err) => {
+        alert(err.message);
+      }
+    );
+  }
+
+  // facebookSignIn() {
+  //   return this.fireauth.signInWithPopup(new FacebookAuthProvider()).then(
+  //     (res) => {
+  //       this.router.navigate(['/dashboard']);
+  //       localStorage.setItem('token', JSON.stringify(res.user?.uid));
+  //     },
+  //     (err) => {
+  //       alert(err.message);
+  //     }
+  //   );
+  // }
+
+  // githubSignIn() {
+  //   return this.fireauth.signInWithPopup(new GithubAuthProvider()).then(
+  //     (res) => {
+  //       this.router.navigate(['/dashboard']);
+  //       localStorage.setItem('token', JSON.stringify(res.user?.uid));
+  //     },
+  //     (err) => {
+  //       alert(err.message);
+  //     }
+  //   );
+  // }
 }
