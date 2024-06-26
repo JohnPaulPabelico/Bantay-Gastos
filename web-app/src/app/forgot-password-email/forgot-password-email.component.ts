6408f85cb942d2cbb8146dea2c3c9f30c868e657
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-26 18:00:40 +0800

    Added a password request page

---
+++ web-app/src/app/forgot-password-email/forgot-password-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-email',
+  templateUrl: './forgot-password-email.component.html',
+  styleUrls: ['./forgot-password-email.component.scss']
+})
+export class ForgotPasswordEmailComponent {
+
+}
