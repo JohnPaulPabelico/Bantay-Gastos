aa2eb66aec46720e777c8e6a1decdf72a8547cb3
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-26 17:58:32 +0800

    Routes

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { RegisterComponent } from './component/register/register.component';
+import { ExpensesComponent } from './component/expenses/expenses.component';
+import { IncomeComponent } from './component/income/income.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { ForgotPasswordEmailComponent } from './forgot-password-email/forgot-password-email.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: 'income', component: IncomeComponent },
+  { path: 'forgot', component: ForgotPasswordComponent },
+  { path: 'verify', component: VerifyEmailComponent },
+  { path: 'forgot/link', component: ForgotPasswordEmailComponent },
 ];
 
 @NgModule({
