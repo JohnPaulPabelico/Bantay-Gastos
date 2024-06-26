aeb6b217f9c0ad842be081b7b9c016970a0f5833
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-06-26 17:59:01 +0800

    Added dashboard

--- web-app/src/app/component/dashboard/dashboard.component.ts
+++ web-app/src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  constructor(private auth: AuthService) {}
 
+  logout() {
+    this.auth.logout();
+  }
 }
