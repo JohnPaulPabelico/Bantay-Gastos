193003236e52a60f1b87a3ace656f09908b025f3
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-07-02 15:14:30 +0800

    Finished both income and expenses charts

--- web-app/src/app/component/dashboard/dashboard.component.ts
+++ web-app/src/app/component/dashboard/dashboard.component.ts
@@ -181,8 +181,9 @@ export class DashboardComponent {
     const expensesAmount = monthlyExpenseTotals.map((item) => item.totalAmount);
     const incomeAmount = monthlyIncomeTotals.map((item) => item.totalAmount);
     const expensesLabels = monthlyExpenseTotals.map((item) => item.monthYear);
+    const incomeLabels = monthlyIncomeTotals.map((item) => item.monthYear);
 
-    new Chart('myChart', {
+    new Chart('expensesChart', {
       type: 'line',
       data: {
         labels: expensesLabels,
@@ -194,11 +195,27 @@ export class DashboardComponent {
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
           },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    new Chart('incomeChart', {
+      type: 'line',
+      data: {
+        labels: incomeLabels,
+        datasets: [
           {
             label: 'Income',
             data: incomeAmount,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
+            backgroundColor: 'rgba(96, 160, 0, 0.2)',
+            borderColor: 'rgba(96, 160, 0, 1)',
             borderWidth: 1,
           },
         ],
