0baa01d55c50ae1fa5510072346fbbe76774593f
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-07-17 17:41:55 +0800

    Updated app state

--- web-app/src/app/state/app.state.ts
+++ web-app/src/app/state/app.state.ts
@@ -1,5 +1,9 @@
 import { ExpensesState } from './expenses/expenses.state';
+import { UserState } from './users/user.state';
+import { IncomeState } from './income/income.state';
 
 export interface AppState {
   Expenses: ExpensesState;
+  User: UserState;
+  Income: IncomeState;
 }
